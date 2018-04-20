2532eefdc6a60ebf95fc278e2792f64a9c8ba2cb
ele_hopper <elenacerezo83@gmail.com>
2018-04-20 10:41:55 +0200

    first launch

---
+++ testingAllonge/JASMINE/spec/appSpec.js
@@ -0,0 +1,15 @@
+describe("test", function () {
+
+
+
+    it("tests arrays boolean", function () {
+
+    });
+
+    it('tests object boolean', function () {
+
+
+    });
+
+
+});
---
+++ testingAllonge/JASMINE/src/app.js
@@ -0,0 +1,5 @@
+function startApp() {
+    'use strict';
+
+
+}
